dbad9ef6cd77bd7df18d9023404aaf9e4a66c4bd
Michał Wójcik <bronybracket@gmail.com>
2020-06-05 18:57:46 +0200

    Updates

---
+++ precache-manifest.29ffe3b9a1e07769e1bbc3f33781ad5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a008e621caca67b2f9aa7b7e0972bc3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e17bc7fa51d25c2650c",
+    "url": "/static/css/2.42e275dd.chunk.css"
+  },
+  {
+    "revision": "b2ab7d826ef35bd666b8",
+    "url": "/static/css/main.3fb313a7.chunk.css"
+  },
+  {
+    "revision": "3e17bc7fa51d25c2650c",
+    "url": "/static/js/2.aa18665c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.aa18665c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2ab7d826ef35bd666b8",
+    "url": "/static/js/main.f4f4408f.chunk.js"
+  },
+  {
+    "revision": "2f59dcffc7f76a7db432",
+    "url": "/static/js/runtime-main.0db0b3eb.js"
+  }
+]);
