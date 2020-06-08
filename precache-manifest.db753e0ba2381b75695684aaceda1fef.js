816db832ead799f61ed7878c932eee81f5309d67
Michał Wójcik <bronybracket@gmail.com>
2020-06-08 12:13:56 +0200

    Updates

--- precache-manifest.db753e0ba2381b75695684aaceda1fef.js
+++ precache-manifest.db753e0ba2381b75695684aaceda1fef.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a008e621caca67b2f9aa7b7e0972bc3d",
+    "revision": "447e49e3f34a42d5869c4f43cadaff87",
     "url": "/index.html"
   },
   {
-    "revision": "3e17bc7fa51d25c2650c",
+    "revision": "a1fdae63d7c79089a040",
     "url": "/static/css/2.42e275dd.chunk.css"
   },
   {
-    "revision": "b2ab7d826ef35bd666b8",
+    "revision": "c1a72dcebc53e0d1f6e5",
     "url": "/static/css/main.3fb313a7.chunk.css"
   },
   {
-    "revision": "3e17bc7fa51d25c2650c",
-    "url": "/static/js/2.aa18665c.chunk.js"
+    "revision": "a1fdae63d7c79089a040",
+    "url": "/static/js/2.7c5e7204.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/static/js/2.aa18665c.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.7c5e7204.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b2ab7d826ef35bd666b8",
-    "url": "/static/js/main.f4f4408f.chunk.js"
+    "revision": "c1a72dcebc53e0d1f6e5",
+    "url": "/static/js/main.5ab70f85.chunk.js"
   },
   {
     "revision": "2f59dcffc7f76a7db432",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.29ffe3b9a1e07769e1bbc3f33781ad5c.js"
+  "/precache-manifest.db753e0ba2381b75695684aaceda1fef.js"
 );
 
 self.addEventListener('message', (event) => {
